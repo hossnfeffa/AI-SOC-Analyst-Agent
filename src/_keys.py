f1af76a003edf508589c85e878a4a2850c5ba258
Brent <brentpetty@comcast.net>
2026-09-18 12:56:47 -0600

    Add files via upload

---
+++ src/_keys.py
@@ -0,0 +1,5 @@
+# https://platform.openai.com/settings/organization/api-keys
+OPENAI_API_KEY = "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"
+
+# https://portal.azure.com/#@lognpacific.com/resource/subscriptions/3c95e63a-895a-4386-991e-edbbf57de5c8/resourceGroups/Cyber-Range-Admin-SOC/providers/Microsoft.OperationalInsights/workspaces/LAW-Cyber-Range/Overview
+LOG_ANALYTICS_WORKSPACE_ID = "60c7f53e-249a-4077-b68e-55a4ae877d7c"
